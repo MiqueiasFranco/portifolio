b88d5818c67a2fd6627de8fbf3b3506e3f95b369
unknown <miqueiassantos1552@gmail.com>
2025-08-09 00:10:40 -0300

    alteraçoes de layout

--- www/js/routes.js
+++ www/js/routes.js
@@ -30,7 +30,7 @@ var app = new Framework7({
 		},
 		pageInit: function (event, page) {
 		// fazer algo quando a página for inicializada
-      app.views.main.router.navigate('/sobre/')
+      //app.views.main.router.navigate('/sobre/')
 		},
 		pageBeforeRemove: function (event, page) {
 		// fazer algo antes da página ser removida do DOM
